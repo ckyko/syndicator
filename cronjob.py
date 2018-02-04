3367628f24f4c8fda3fa4efc03efc4fee0d83bea
kaiying <ckykokoko@gmail.com>
2018-02-04 02:57:17 -0500

    try call API for cronjob

---
+++ cronjob.py
@@ -0,0 +1,5 @@
+
+import requests
+
+url = 'http://django-syn.ewuen2g2bs.us-east-1.elasticbeanstalk.com/management/run'
+response = requests.get(url=url)
--- management/views.py
+++ management/views.py
@@ -8,7 +8,6 @@ from .cron import MyCronJob
 
 
 # Create your views here.
-@login_required
 def index(request):
     cron_job = MyCronJob()
     cron_job.do()
